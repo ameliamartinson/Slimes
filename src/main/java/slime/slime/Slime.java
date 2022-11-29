d5e9439b17c3d3e3e733f0dad1c6bb7d28a68402
joemartinson <martinsonjoe2@gmail.com>
2022-11-29 12:57:37 -0800

    update to 1.19.2

--- src/main/java/slime/slime/Slime.java
+++ src/main/java/slime/slime/Slime.java
@@ -1,18 +1,19 @@
 package slime.slime;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import static net.minecraft.server.command.CommandManager.literal;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
+import static net.minecraft.server.command.CommandManager.*;
+import net.minecraft.text.Text;
 
 import java.util.Random;
 
 public class Slime implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("slime").executes(context -> {
                 ServerCommandSource source = context.getSource();
                 ServerPlayerEntity player = source.getPlayer();
@@ -30,9 +31,9 @@ public class Slime implements ModInitializer {
 
                 boolean isSlimeChunk = (rnd.nextInt(10) == 0);
                 if (isSlimeChunk) {
-                    source.sendFeedback(new LiteralText("Chunk " + xPosition + ", " + zPosition + " is a slime chunk."), false);
+                    source.sendFeedback(Text.literal("Chunk " + xPosition + ", " + zPosition + " is a slime chunk."), false);
                 } else {
-                    source.sendFeedback(new LiteralText("Chunk " + xPosition + ", " + zPosition + " is NOT a slime chunk."), false);
+                    source.sendFeedback(Text.literal("Chunk " + xPosition + ", " + zPosition + " is NOT a slime chunk."), false);
                 }
                 return 1;
             }));
