a273deb137c41dbf6b5fbd5a7ebfac201ce7327a
Amelia Martinson <ameliamartinson01@gmail.com>
2023-11-11 18:38:09 -0800

    initial commit

---
+++ src/main/java/slime/slime/Slime.java
@@ -0,0 +1,42 @@
+package slime.slime;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import static net.minecraft.server.command.CommandManager.literal;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+
+import java.util.Random;
+
+public class Slime implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            dispatcher.register(literal("slime").executes(context -> {
+                ServerCommandSource source = context.getSource();
+                ServerPlayerEntity player = source.getPlayer();
+                int xPosition = player.getChunkPos().x;
+                int zPosition = player.getChunkPos().z;
+                long seed = source.getWorld().getSeed();
+
+                Random rnd = new Random(
+                        seed +
+                                (int) (xPosition * xPosition * 0x4c1906) +
+                                (int) (xPosition * 0x5ac0db) +
+                                (int) (zPosition * zPosition) * 0x4307a7L +
+                                (int) (zPosition * 0x5f24f) ^ 0x3ad8025fL
+                );
+
+                boolean isSlimeChunk = (rnd.nextInt(10) == 0);
+                if (isSlimeChunk) {
+                    source.sendFeedback(new LiteralText("Chunk " + xPosition + ", " + zPosition + " is a slime chunk."), false);
+                } else {
+                    source.sendFeedback(new LiteralText("Chunk " + xPosition + ", " + zPosition + " is NOT a slime chunk."), false);
+                }
+                return 1;
+            }));
+        });
+    }
+}
---
+++ src/main/java/slime/slime/client/SlimeClient.java
@@ -0,0 +1,13 @@
+package slime.slime.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SlimeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
